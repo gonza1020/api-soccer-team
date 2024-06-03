d301a03d7f6144731213306d46a57bcffc4a0528
Gonzalo Zoloaga <gonzalozoloaga44@gmail.com>
2024-06-03 19:45:41 -0300

    Usuario para manejar autenticacion

---
+++ src/main/java/com/dux/api_soccer_team/dto/User.java
@@ -0,0 +1,14 @@
+package com.dux.api_soccer_team.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+    private String username;
+    private String password;
+
+  }
